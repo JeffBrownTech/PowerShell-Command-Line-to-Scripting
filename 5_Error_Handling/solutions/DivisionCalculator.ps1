e55aeda55ede0e19d4fef3b2eb92845186b92a4d
Jeff Brown <jeff@jeffbrown.tech>
2025-08-17 08:19:08 -0700

    Add Catch Block

--- 5_Error_Handling/solutions/DivisionCalculator.ps1
+++ 5_Error_Handling/solutions/DivisionCalculator.ps1
@@ -14,6 +14,9 @@ Try {
 Catch [System.DivideByZeroException] {
     Write-Warning "You tried to divide by zero!"
 }
+Catch [System.FormatException] {
+    Write-Warning "Could not parse into an integer. Please enter a valid integer value (e.g. 1, 2, 3...)"
+}
 Catch {
     # Handle errors
     Write-Output "Exception Type: $($_.Exception.InnerException.GetType().FullName)"
